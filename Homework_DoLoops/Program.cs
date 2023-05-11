bd9c169e44e8e979c2ae815cc925eab208cef9ea
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-10 23:59:12 -0600

    Add project files.

---
+++ Homework_DoLoops/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// Create a Console Application that asks user for their name.
+// Welcome me (Tim) as professor and anyone else as student.
+// Do this until the user types "exit"
+
+//ask user for their name
+Console.Write("What is your name: ");
